54fa230a1c7f23c1812f148afad1092ded89a636
Ronny Reinhold <ronny@bnu.com.br>
2017-02-07 23:42:35 -0200

    Created gig methods and views for new and show page

--- app/controllers/gigs_controller.rb
+++ app/controllers/gigs_controller.rb
@@ -1,6 +1,36 @@
 class GigsController < ApplicationController
+  before_action :set_gig, only: [:show, :edit, :update, :delete]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
+
   end
 
+  def new
+    @gig = Gig.new
+  end
+
+  def create
+    @gig = current_user.gigs.build(gig_params)
+
+    if @gig.save
+      redirect_to @gig, notice: "Job created successfully"
+    else
+      render :new
+    end
+  end
+
+  def show
+
+  end
+
+  private
+
+    def gig_params
+      params.require(:gig).permit(:name, :description, :budget, :location, :open, :awarded_proposal, :category_id, :user_id)
+    end
+
+    def set_gig
+      @gig = Gig.find(params[:id])
+    end
 end
